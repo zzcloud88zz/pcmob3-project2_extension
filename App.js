4492841423b030001b841d271120653b15e61a24
shengrong <shengrong_88@hotmail.com>
2020-12-10 12:30:26 +0800

    added header right icon button

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StyleSheet, Text, View, Button } from 'react-native';
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
+import { Entypo } from '@expo/vector-icons';
 import * as SQLite from 'expo-sqlite';
 
 function HomeScreen({ navigation }) {
+  function message(){
+    return console.log("lai lai lai, Durian $1!!!")
+  }
+
+  useEffect(() => {
+    navigation.setOptions({
+      headerRight: () => <Entypo onPress={message} name="new-message" size={50} color="black" />
+    });
+  });
+
   return (
     <View style={styles.container}>
       <Text>Homepage</Text>
