b7def5b4ce78482624b4f6c9966a926d7af56679
shengrong <shengrong_88@hotmail.com>
2020-12-10 12:16:32 +0800

    added stack nav & header styling

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer } from "@react-navigation/native";
 import * as SQLite from 'expo-sqlite';
 
-export default function App() {
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Homepage</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen 
+        options={{
+        headerStyle: {
+          height: 120,
+          backgroundColor: "yellow",
+          borderBottomColor: "#ccc",
+          borderBottomWidth: 1,
+        },
+      }}
+        name="Notes App"
+        component={HomeScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+// styling
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightyellow',
     alignItems: 'center',
     justifyContent: 'center',
   },
